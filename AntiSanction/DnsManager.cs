959948a12bdc72b58c535bd10f7f5a6bf1066b74
mirmostafa <mirmostafa@hotmail.com>
2024-12-08 10:12:19 +0330

    Initial commit

---
+++ AntiSanction/DnsManager.cs
@@ -0,0 +1,127 @@
+﻿using System.Diagnostics;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace AntiSanction;
+
+public class DnsManager
+{
+    /// <summary>
+    /// Changes DNS settings for the specified network interface.
+    /// </summary>
+    /// <param name="interfaceId">The unique ID of the network interface.</param>
+    /// <param name="dnsAddresses">The new DNS addresses to apply.</param>
+    public static void ChangeDnsByInterfaceId(string interfaceId, params string[] dnsAddresses)
+    {
+        var networkInterface = NetworkInterface.GetAllNetworkInterfaces()
+            .FirstOrDefault(ni => ni.Id == interfaceId) ?? throw new ArgumentException("Network interface not found.");
+        var processStartInfo = new ProcessStartInfo
+        {
+            FileName = "netsh",
+            Arguments = $"interface ip set dns \"{networkInterface.Name}\" static {dnsAddresses[0]}",
+            Verb = "runas",
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        ExecuteProcess(processStartInfo);
+
+        if (dnsAddresses.Length > 1)
+        {
+            for (int i = 1; i < dnsAddresses.Length; i++)
+            {
+                processStartInfo.Arguments = $"interface ip add dns \"{networkInterface.Name}\" {dnsAddresses[i]} index={i + 1}";
+                ExecuteProcess(processStartInfo);
+            }
+        }
+
+        // Flush the DNS cache
+        var flushDnsProcess = new ProcessStartInfo
+        {
+            FileName = "ipconfig",
+            Arguments = "/flushdns",
+            Verb = "runas",
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        ExecuteProcess(flushDnsProcess);
+    }
+
+    /// <summary>
+    /// Gets the current DNS settings for the specified network interface.
+    /// </summary>
+    /// <param name="interfaceId">The unique ID of the network interface.</param>
+    /// <returns>List of current DNS addresses.</returns>
+    public static IEnumerable<string> GetCurrentDnsByInterfaceId(string interfaceId)
+    {
+        var networkInterface = NetworkInterface.GetAllNetworkInterfaces()
+            .FirstOrDefault(ni => ni.Id == interfaceId) ?? throw new ArgumentException("Network interface not found.");
+        var ipProperties = networkInterface.GetIPProperties();
+        return ipProperties.DnsAddresses
+            .Where(ip => ip.AddressFamily == AddressFamily.InterNetwork)
+            .Select(ip => ip.ToString());
+    }
+
+    /// <summary>
+    /// Retrieves a list of available network interfaces.
+    /// </summary>
+    /// <returns>List of network interfaces.</returns>
+    public static IEnumerable<NetworkInterfaceInfo> GetInterfaces() => 
+        NetworkInterface.GetAllNetworkInterfaces()
+            .Select(ni => new NetworkInterfaceInfo
+            {
+                Id = ni.Id,
+                Name = ni.Name,
+                Description = ni.Description
+            });
+
+    /// <summary>
+    /// Resets DNS settings for the specified network interface to automatic (DHCP).
+    /// </summary>
+    /// <param name="interfaceId">The unique ID of the network interface.</param>
+    public static void ResetDns(string interfaceId)
+    {
+        var networkInterface = NetworkInterface.GetAllNetworkInterfaces()
+            .FirstOrDefault(ni => ni.Id == interfaceId) ?? throw new ArgumentException("Network interface not found.");
+        var processStartInfo = new ProcessStartInfo
+        {
+            FileName = "netsh",
+            Arguments = $"interface ip set dns \"{networkInterface.Name}\" dhcp",
+            Verb = "runas",
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        ExecuteProcess(processStartInfo);
+    }
+
+    /// <summary>
+    /// Executes a system command with the specified settings.
+    /// </summary>
+    /// <param name="processStartInfo">Process start information.</param>
+    private static void ExecuteProcess(ProcessStartInfo processStartInfo)
+    {
+        using var process = Process.Start(processStartInfo) ?? throw new InvalidOperationException("Failed to start process.");
+        process.WaitForExit();
+
+        if (process.ExitCode != 0)
+        {
+            var error = process.StandardError.ReadToEnd();
+            throw new InvalidOperationException($"Command failed: {error}");
+        }
+    }
+
+    public class NetworkInterfaceInfo
+    {
+        public required string Description { get; set; }
+        public required string Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
---
+++ AntiSanction/Program.cs
@@ -0,0 +1,93 @@
+﻿using static AntiSanction.DnsManager;
+
+try
+{
+    var ethernet = GetInterfaces().First(x => x.Name == "Ethernet");
+    if (args.Length == 0)
+    {
+        Console.WriteLine("Re/set DNS Server to Shekan.");
+        Console.WriteLine();
+        Console.WriteLine("  (1): Show Current DNS.");
+        Console.WriteLine("  (2): Set to shekan.");
+        Console.WriteLine("  (3): Reset to default DNS (192.168.1.1).");
+        Console.WriteLine("  (?): Show help.");
+        Console.WriteLine("  Other keys: Exit.");
+        Console.Write("Choose an item: ");
+        var resp = Console.ReadKey();
+        Console.WriteLine();
+        if (resp.Key is ConsoleKey.D1 or ConsoleKey.NumPad1)
+        {
+            showCurrentDns(ethernet);
+        }
+        if (resp.Key is ConsoleKey.D2 or ConsoleKey.NumPad2)
+        {
+            setToShekan(ethernet);
+        }
+        if (resp.Key is ConsoleKey.D3 or ConsoleKey.NumPad3)
+        {
+            resetDns(ethernet);
+        }
+        if (resp.Key is ConsoleKey.Oem2 or ConsoleKey.H)
+        {
+            showHelp();
+        }
+
+        return;
+    }
+    if (args.Length == 1)
+    {
+        if (args[0] is "--info" or "-i" or "i")
+        {
+            showCurrentDns(ethernet);
+            return;
+        }
+        if (args[0] is "--reset" or "-r" or "r")
+        {
+            resetDns(ethernet);
+            return;
+        }
+        if (args[0] is "--set" or "-s" or "s")
+        {
+            setToShekan(ethernet);
+            return;
+        }
+        if (args[0] is "--help" or "-h" or "h")
+        {
+            showHelp();
+            return;
+        }
+    }
+    showHelp();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
+
+static void showCurrentDns(NetworkInterfaceInfo ethernet)
+{
+    var currentDnses = GetCurrentDnsByInterfaceId(ethernet.Id);
+    foreach (var curr in currentDnses)
+    {
+        Console.WriteLine(curr);
+    }
+}
+
+static void resetDns(NetworkInterfaceInfo ethernet)
+{
+    ChangeDnsByInterfaceId(ethernet.Id, "192.168.1.1");
+    Console.WriteLine("Reset.");
+}
+
+static void setToShekan(NetworkInterfaceInfo ethernet)
+{
+    ChangeDnsByInterfaceId(ethernet.Id, "178.22.122.100", "185.51.200.2");
+    Console.WriteLine("Changed to Shekan.");
+}
+
+static void showHelp()
+{
+    Console.WriteLine("--info\t-i i : Show current DNS.");
+    Console.WriteLine("--set\t-s s : Set DNS to Shekan.");
+    Console.WriteLine("--reset\t-r r : Reset DNS to default.");
+}
